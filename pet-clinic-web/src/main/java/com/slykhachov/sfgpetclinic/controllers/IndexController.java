63a1e87e7afb5d914bcb85d0da0d5be9b9acd2d5
SergeyLykhachov <lykhachovs@gmail.com>
2019-08-31 16:38:24 +0300

    Fixes Broken Links

--- pet-clinic-web/src/main/java/com/slykhachov/sfgpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/com/slykhachov/sfgpetclinic/controllers/IndexController.java
@@ -1,6 +1,5 @@
 package com.slykhachov.sfgpetclinic.controllers;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,5 +9,10 @@ public class IndexController {
     @RequestMapping({"", "/", "index", "index.html"})
     public String index() {
         return "index";
+    }
+
+    @RequestMapping("/oups")
+    public String oupsHandler() {
+        return "notimplemented";
     }
 }
