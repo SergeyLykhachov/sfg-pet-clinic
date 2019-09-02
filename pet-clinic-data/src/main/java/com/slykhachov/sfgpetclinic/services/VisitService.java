dc0bd81bdaaa72935c01625c9f94b4ec9ee37fe5
SergeyLykhachov <lykhachovs@gmail.com>
2019-09-03 00:14:04 +0300

    Adds Map Implementation for Visit Service.

---
+++ pet-clinic-data/src/main/java/com/slykhachov/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.slykhachov.sfgpetclinic.services;
+
+import com.slykhachov.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/slykhachov/sfgpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,39 @@
+package com.slykhachov.sfgpetclinic.services.map;
+
+import com.slykhachov.sfgpetclinic.model.Visit;
+import com.slykhachov.sfgpetclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getId() == null || visit.getPet().getOwner() == null
+                || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException();
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit obj) {
+        super.delete(obj);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
