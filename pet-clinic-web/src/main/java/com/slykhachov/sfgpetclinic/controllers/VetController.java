ebaad7de532637282c370884659375a2f12ea78c
SergeyLykhachov <lykhachovs@gmail.com>
2019-09-16 23:08:58 +0300

    Adds JSON End Point.

--- pet-clinic-web/src/main/java/com/slykhachov/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/slykhachov/sfgpetclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.slykhachov.sfgpetclinic.controllers;
 
+import com.slykhachov.sfgpetclinic.model.Vet;
 import com.slykhachov.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -18,5 +23,11 @@ public class VetController {
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    @ResponseBody
+    public Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
